f64d41dae1fed5e160c2727e1c7e283f70320d34
Harsimar Ahluwalia <harsimar.ahluwalia@gmail.com>
2017-04-26 20:09:38 -0400

    Update main.js

--- Final Project/www/js/main.js
+++ Final Project/www/js/main.js
@@ -1,13 +1,30 @@
-$( document ).ready(function() {
-    
+$( document ).ready(function() {   
 $('input').click(function () {
-$.getJSON('http://horoscope-api.herokuapp.com/horoscope/today/'+this.id, function (data) {
+    /*
+$.getJSON('http://horoscope-api.herokuapp.com/horoscope/today/'+this.id+'?callback=?', function (data) {
       var dailyHoroscope = data.horoscope;
       var dateToday = data.date;
       var zodiac = data.sunsign;
         $("#zodiac").html(zodiac);
         $("#date").html(dateToday);
         $("#horoscope").html(dailyHoroscope);
+    });
+    */
+    $.ajax({
+      crossOrigin: true,
+      url : "http://horoscope-api.herokuapp.com/horoscope/today/"+this.id,
+      type : "GET",
+      success:function(data){
+        
+        var jsonObject = JSON.parse(data);
+
+        var dailyHoroscope = jsonObject.horoscope;
+        var dateToday = jsonObject.date;
+        var zodiac = jsonObject.sunsign;
+        $("#zodiac").html(zodiac);
+        $("#date").html(dateToday);
+        $("#horoscope").html(dailyHoroscope);
+      }
     });
   });
   });
